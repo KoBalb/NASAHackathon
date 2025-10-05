535b2e6503808e4a664902b4c9f5e8c84bb21075
Artem <zalizkoartem28@gmail.com>
2025-10-05 21:21:11 +0300

    Анімації. (#32)

--- src/pages/astronauts/Astronauts.tsx
+++ src/pages/astronauts/Astronauts.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import AstroBar from "../../components/astronauts/AstroCards/AstroBar";
 import AstroSearchBar from "../../components/astronauts/AstroSearchBar/AstroSearchBar";
 import NavigationBar from "../../components/general/NavBar";
@@ -7,8 +8,10 @@ function Astronauts() {
     <div className="home-page">
       <NavigationBar />
       <main>
-        <AstroSearchBar />
-        <AstroBar />
+        <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>
+          <AstroSearchBar />
+          <AstroBar />
+        </motion.div>
       </main>
     </div>
   );
--- src/pages/catalogs/Catalogs.tsx
+++ src/pages/catalogs/Catalogs.tsx
@@ -2,6 +2,7 @@ import NavigationBar from "../../components/general/NavBar";
 import "../../components/general/PageStyles.css";
 import CatalogSearchBar from "../../components/catalogs/SearchBar/CatalogSearchBar";
 import CatalogBar from "../../components/catalogs/CatalogsCard/CatalogBar";
+import { motion } from "framer-motion";
 
 
 function Catalogs() {
@@ -9,8 +10,10 @@ function Catalogs() {
         <div className="home-page">
             <NavigationBar />
             <main>
-                <CatalogSearchBar />
-				<CatalogBar />
+                <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>
+                    <CatalogSearchBar />
+				    <CatalogBar />
+                </motion.div>
             </main>
         </div>
     )
