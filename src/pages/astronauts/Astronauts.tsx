bc5eb5ceabcd6b0b7519db72c88e0534dc2eb808
KoBalb <artem.kovalyk79@gmail.com>
2025-10-05 17:30:20 +0300

    1

--- src/pages/astronauts/Astronauts.tsx
+++ src/pages/astronauts/Astronauts.tsx
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import ModuleForm from "../../components/redactorForm/redactorForm";
-import ResourceManager from "../../components/resourcesCard/ResourcesCard";
-import Modal from "../../components/redactorForm/redactorForm";
 
-
-function Astronauts() {
-        
-
- return (
-    <div className="redactor-container">
-      <div className="redactor-left">
-        <ModuleForm />
-      </div>
-      <div className="redactor-right">
-        <ResourceManager />
-      </div>
-    </div>
-  );
-};
-
-export default Astronauts;
-=======
 import NavigationBar from "../../components/general/NavBar";
 import AstroBar from "../../components/astronauts/AstroCards/AstroBar";
 import AstroSearchBar from "../../components/astronauts/AstroSearchBar/AstroSearchBar";
@@ -38,4 +15,3 @@ function Astronauts() {
   );
 }
 export default Astronauts;
->>>>>>> 1b7fcca29ea1809569e43b7172fb44e43c710968
