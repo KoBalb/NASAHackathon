52b9c94d8cb544806da63c06369877aebb898dcd
KoBalb <artem.kovalyk79@gmail.com>
2025-10-05 13:49:53 +0300

    das

--- src/pages/astronauts/Astronauts.tsx
+++ src/pages/astronauts/Astronauts.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
+import ResourceForm from "../../components/ResourceForm/Resourceform";
 
 
 function Astronauts() {
-        return (
-        <div>
-            <h1>Astronauts Page</h1>
-        </div>
-    );
-};
+        
 
+    const [modalOpen, setModalOpen] = useState(false);
+    return( <>
+      <button onClick={() => setModalOpen(true)}>
+        Дasdddddddddddddddddd
+      </button>
+        
+      {modalOpen && (
+        <ResourceForm 
+          onClose={() => setModalOpen(false)} 
+        />
+      )}
+    </>
+  );
+}
 export default Astronauts;
