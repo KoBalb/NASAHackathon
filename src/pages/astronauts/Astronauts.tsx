1f08d9c900d21724e65742e34482ed738d8b627b
KoBalb <artem.kovalyk79@gmail.com>
2025-10-05 17:37:11 +0300

    asd

--- src/pages/astronauts/Astronauts.tsx
+++ src/pages/astronauts/Astronauts.tsx
@@ -1,14 +1,8 @@
+import AstroBar from "../../components/astronauts/AstroCards/AstroBar";
+import AstroSearchBar from "../../components/astronauts/AstroSearchBar/AstroSearchBar";
+import NavigationBar from "../../components/general/NavBar";
 
 function Astronauts() {
-  return (
-    <div className="home-page">
-      <NavigationBar />
-      <main>
-        <AstroSearchBar />
-        <AstroBar />
-      </main>
-    </div>
-  );
   return (
     <div className="home-page">
       <NavigationBar />
